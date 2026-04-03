f6d5745ec05fdea8991fd350b6a6013e7393fdf9
Lilya Yenokyan <lilya.yenokyan.techX@gmail.com>
2026-04-03 16:18:52 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  interface User{
+    id: number,
+    name: string,
+    title: string,
+    imgUrl: string,
+  }
+  const [users,setUsers] = useState<User[]>([])//empty array for now
+  
   return (
     <div>
-      
+      <h1>Our Team</h1>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <h2></h2>
+          <p></p>
+        </div>
+      </div>
     </div>
   )
 }
